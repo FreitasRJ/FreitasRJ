aa4e32bf65f31acd4d427d38bcbb280c7bd57803
Fabio Freitas <fabiofsrj@gmail.com>
2022-06-30 11:49:37 -0300

    modificação no programa separa_inteiro: aceita numeros float com várias casas além do ponto.

--- experimentos/separa_inteiro.py
+++ experimentos/separa_inteiro.py
@@ -12,23 +12,31 @@ def alinha_saida_valores_no_print(numero: float, num_caracteres: int):
     inteiro = ''
 
     while posicao < tamanho and num_str[posicao] != '.':
+        inteiro += num_str[posicao]
         posicao += 1
+        print(inteiro)
+        
         
     if posicao == tamanho:
         num_str += '.00'
     
-    if posicao == tamanho -2:
+    elif posicao == tamanho - 2:
         num_str += '0'
+    
+    elif num_str[posicao] == '.' and tamanho - posicao - 1 >= 2:
+        num_str = inteiro + num_str[posicao] + num_str[posicao + 1] + num_str[posicao + 2]       
        
+ 
     num_espacos = num_caracteres - posicao
     num_espacos_str = '_' * num_espacos
 
     saida = num_espacos_str + num_str  
     return saida
 
+
 valor = alinha_saida_valores_no_print(1225.1, 6)
 valor2 = alinha_saida_valores_no_print(25, 6)
-valor3 = alinha_saida_valores_no_print(25.3333, 6)
+valor3 = alinha_saida_valores_no_print(25.333, 6)
 print(valor)
 print(valor2)
 print(valor3)
