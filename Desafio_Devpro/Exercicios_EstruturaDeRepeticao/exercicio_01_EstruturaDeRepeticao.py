ef4e672eddeb900048d5c4e1896ad53b84521465
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-06 03:18:59 -0300

    inclusao da resposta do exercicio 02 da lista estrutura de decisao

--- Desafio_Devpro/Exercicios_EstruturaDeRepeticao/exercicio_01_EstruturaDeRepeticao.py
+++ Desafio_Devpro/Exercicios_EstruturaDeRepeticao/exercicio_01_EstruturaDeRepeticao.py
@@ -1,6 +1,6 @@
 '''
-1 - Faça um programa que peça uma nota, entre zero e dez. 
-Mostre uma mensagem caso o valor seja inválido e continue 
+1 - Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido e continue
 pedindo até que o usuário informe um valor valido.
 '''
 
@@ -12,7 +12,7 @@ while True:
             if nota < 0 or nota > 10:
                 print('Valor inválido!')
         break
-        
+
     except ValueError:
         break
 
