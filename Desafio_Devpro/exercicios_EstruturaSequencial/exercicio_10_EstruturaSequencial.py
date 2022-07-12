d1902488d02a8d46ff7af5d51148b130fa133770
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-12 09:28:03 -0300

    arredondamento  do calculo.

--- Desafio_Devpro/exercicios_EstruturaSequencial/exercicio_10_EstruturaSequencial.py
+++ Desafio_Devpro/exercicios_EstruturaSequencial/exercicio_10_EstruturaSequencial.py
@@ -9,8 +9,9 @@ Fórmula
 while True:
         try:
             cel = float(input('Informe a temperatura em Celsius a ser convertida em Fahrenheit: '))
-            # print(f'O equivalente a {cel} Fahrenheit em Fahrenheit é { (cel * (9/5)) + 32}')
-            print(f'Essa temperatura é de {(cel * (9/5)) + 32} Fahrenheit')
+            fahrenheit = round((cel * (9/5)) + 32)
+            print(f'Essa temperatura é de {fahrenheit} Fahrenheit')
+            break
         except ValueError:
             print('Valor informado ínválido!!!')
         
