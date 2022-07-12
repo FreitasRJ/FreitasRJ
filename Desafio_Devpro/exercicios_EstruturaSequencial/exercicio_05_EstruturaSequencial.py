9dc4e488e371d6581610c0ebfbe8214193a9b4e9
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-12 09:10:49 -0300

    inclusão de break na linha 11 e forçar perguntar um a vez.

--- Desafio_Devpro/exercicios_EstruturaSequencial/exercicio_05_EstruturaSequencial.py
+++ Desafio_Devpro/exercicios_EstruturaSequencial/exercicio_05_EstruturaSequencial.py
@@ -8,6 +8,7 @@ while True:
         metro = float(input('Informe a medida em metros a ser convertida em centimetros: '))
         centimetro = metro * 100        
         print(f'Transformando para centímetros dá {centimetro} cm')
+        break # Adiciondo para só perguntar uma vez e não prejudicar doctest.
     except ValueError:
         print('Não informou um número!!!')
         
