1ba21a46154f9d4b93440e95586cdcd73496a070
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-12 15:22:57 -0300

    inclui aspas simples nas saida e retirei o loop while True:

--- Desafio_Devpro/exercicios_EstruturaDeDecisao/exercicio_13_EstruturaDeDecisao.py
+++ Desafio_Devpro/exercicios_EstruturaDeDecisao/exercicio_13_EstruturaDeDecisao.py
@@ -9,30 +9,30 @@ se digitar outro valor deve aparecer valor inválido.
     
 """
 
-while True:
-    try:
-        # dia = 0
-        dia = int(input('Digite um número entre 1 e 7: '))
-      
-      
-        if  dia < 1 or dia >7:
-            print('Dia Inválido')
 
-        elif dia == 1:
-            print('Domingo')
-        elif dia == 2:
-            print('Segunda')
-        elif dia == 3:
-            print('Terça')
-        elif dia == 4:
-            print('Quarta')
-        elif dia == 5:
-            print('Quinta')
-        elif dia == 6:
-            print('Sexta')
-        elif dia == 7:
-            print('Sábado')
+try:
+    # dia = 0
+    dia = int(input('Digite um número entre 1 e 7: '))
+      
+      
+    if  dia < 1 or dia >7:
+        print("'Dia Inválido'")
+
+    elif dia == 1:
+        print("'Domingo'")
+    elif dia == 2:
+        print("'Segunda'")
+    elif dia == 3:
+        print("'Terça'")
+    elif dia == 4:
+        print("'Quarta'")
+    elif dia == 5:
+        print("'Quinta'")
+    elif dia == 6:
+        print("'Sexta'")
+    elif dia == 7:
+        print("'Sábado'")
         
-    except ValueError:
-        print('Dia Inválido')
+except ValueError:
+    print('Dia Inválido')
         
