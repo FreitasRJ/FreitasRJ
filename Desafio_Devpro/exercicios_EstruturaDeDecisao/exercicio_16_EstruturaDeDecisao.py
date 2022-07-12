4be7a1dd99b389dbe252ab86063cb2e55e1ea9fb
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-12 15:40:03 -0300

    Inclusão aspas simples para saída igual ao testes.

--- Desafio_Devpro/exercicios_EstruturaDeDecisao/exercicio_16_EstruturaDeDecisao.py
+++ Desafio_Devpro/exercicios_EstruturaDeDecisao/exercicio_16_EstruturaDeDecisao.py
@@ -19,7 +19,7 @@ while True:
     try:
         a = float(input('Entre com o valor de a: '))
         if a == 0:
-            print('Valor do coeficiente a deve ser diferente de 0')
+            print("'Valor do coeficiente a deve ser diferente de 0'")
             break
         b = float(input('Entre com o valor de b: '))
         c = float(input('Entre com o valor de c: '))
@@ -27,15 +27,15 @@ while True:
         delta = (b**2) - 4 * a * c
         
         if delta < 0:
-           mensagem = 'Delta negativo ({delta:.0f}), por isso não existem raízes reais'
+           mensagem = f"'Delta negativo ({delta:.0f}), por isso não existem raízes reais'"
           
         elif delta == 0:
             raiz = (-b - math.sqrt(delta))/2 * a
-            mensagem = f'Delta é 0, raíz única no valor de {raiz}'
+            mensagem = f"'Delta é 0, raíz única no valor de {raiz}'"
         else:
             raiz = (-b - math.sqrt(delta))/2 * a
             raiz2 = (-b + math.sqrt(delta))/2 * a
-            mensagem = f'Delta é {delta:.0f}, raízes são {raiz2} e {raiz}'
+            mensagem = f"'Delta é {delta:.0f}, raízes são {raiz2} e {raiz}'"
             
         print(mensagem)
         break
