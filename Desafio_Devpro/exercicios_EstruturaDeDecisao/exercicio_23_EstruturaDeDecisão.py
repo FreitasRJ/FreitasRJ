8a27cf6913b3459678a54fd68b75c09eb6ddce55
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-13 10:58:10 -0300

    inclusão aspas simples na saída.

--- Desafio_Devpro/exercicios_EstruturaDeDecisao/exercicio_23_EstruturaDeDecisão.py
+++ Desafio_Devpro/exercicios_EstruturaDeDecisao/exercicio_23_EstruturaDeDecisão.py
@@ -1,12 +1,17 @@
+'''
+23 - Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de
+arredondamento.
+
+'''
 while True:
     try:
 
         numero = float(input('digite um número: '))
         num = round(numero)
         if numero == num:
-            print('Inteiro')
+            print("'Inteiro'")
         else:
-            print('Decimal') 
+            print("'Decimal'") 
     except ValueError:
         print('Entrada inválida')
         
