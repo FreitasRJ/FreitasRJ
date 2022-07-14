3104d5f9c369676be40ed58c044ba890433bfa5a
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-07-14 19:47:30 -0300

    refatoração do código anterior.

--- Desafio_Devpro/Exercicios_EstruturaDeRepeticao/exercicio_07_EstruturaDeRepeticao.py
+++ Desafio_Devpro/Exercicios_EstruturaDeRepeticao/exercicio_07_EstruturaDeRepeticao.py
@@ -3,19 +3,11 @@
 que leia 5 números e informe
  o maior número.
 '''
-lista = []
-for a in range(1,6):
-    numero = input(f'Informe o número {a}:  ')
-    lista.append(numero)
-   
-a, b, c, d, e = int(lista[0]), int(lista[1]), int(lista[2]), int(lista[3]), int(lista[4])
+try:
+    maior = 0
+    for a in range(1,6):
+        numero = int(input(f'Informe o número {a}:  '))
+        if numero > maior:
+            maior = numero
 
-if a > b:
-    a, b = b, a
-if b > c:
-    b, c = c, b
-if c > d:
-    c, d = d, c
-if d > e:
-    d, e = e, d
-print(f'O maior número da lista é: {e}')
+    print(f'O maior número da lista é: {maior}')
