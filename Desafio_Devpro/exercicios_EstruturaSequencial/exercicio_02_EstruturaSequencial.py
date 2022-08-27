4c026f910f298df73361c30eb4c9b496241d075f
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-08-27 17:00:49 -0300

    retirada da pasta desafio devpro.

--- Desafio_Devpro/exercicios_EstruturaSequencial/exercicio_02_EstruturaSequencial.py
+++
@@ -1,6 +0,0 @@
-'''
-Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-'''
-
-numero = input('Entre com um número: ')
-print(f'O número informado foi {numero}')
