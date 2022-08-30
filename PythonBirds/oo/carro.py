d2149a5aafdcac95ae58f936c6f0836e615a4895
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-08-30 19:50:12 -0300

    feat: Inclusao de simulacao do uso  de um carro.

--- PythonBirds/oo/carro.py
+++ PythonBirds/oo/carro.py
@@ -23,8 +23,9 @@ seguintes atributos:
 O   L
   S
 
-    Exemplo:
-    # testando motor
+
+Exemplo:
+    >>> # Testando motor
     >>> motor = Motor()
     >>> motor.velocidade
     0
@@ -59,25 +60,20 @@ O   L
     >>> direcao.girar_a_direita()
     >>> direcao.valor
     'Norte'
-
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Oeste'
-
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Sul'
-
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Leste'
-
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Norte'
-
     >>> carro = Carro(direcao, motor)
-    >>> carro.calcula_velocidade()
+    >>> carro.calcular_velocidade()
     0
     >>> carro.acelerar()
     >>> carro.calcular_velocidade()
@@ -88,35 +84,53 @@ O   L
     >>> carro.frear()
     >>> carro.calcular_velocidade()
     0
-    >>> carro.calcula_direcao()
-    >>> 'Norte'
-    >>> carro.gira_a_direita()
-    >>> carro.calcula_direcao()
-    >>> 'Leste'
-    >>> carro.gira_a_esquerda()
-    >>> carro.calcula_direcao()
-    >>> 'Norte'
-    >>> carro.gira_a_esquerda()
-    >>> carro.calcula_direcao()
-    >>> 'Oeste'
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_direita()
+    >>> carro.calcular_direcao()
+    'Leste'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Oeste'
 
 '''
+class Carro:
 
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+    
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
 
 
 class Motor:
     def __init__(self):
-        self.velocidade = 0
-        
+        self.velocidade = 0    
 
     def acelerar(self):
-        self.velocidade += 1
-    
+        self.velocidade += 1 
+       
+
     def frear(self):
         self.velocidade -= 2
         if self.velocidade < 0:
             self.velocidade = 0
-    
+       
+
 class Direcao:
     
     def __init__(self):
@@ -130,7 +144,29 @@ class Direcao:
             self.chave = 1
         self.valor = rumo[self.chave]
 
+    def girar_a_esquerda(self):
+        rumo = {1:'Norte', 2:'Leste', 3:'Sul', 4:'Oeste'}
+        self.chave -=1
+        if self.chave < 1:
+            self.chave = 4
+        self.valor = rumo[self.chave]
+        
 
-a = Direcao()
-a.girar_a_direita()
-print(a.valor)
+# Teste
+car = Carro(Direcao(),Motor())
+
+while True:
+
+    print(f'Direção: {car.calcular_direcao():<5}  -  Velocidade: {car.calcular_velocidade():>3}    ', end="")
+    acao = input()
+
+    if acao == '4':
+        car.direcao.girar_a_esquerda()            
+    elif acao == '6':
+        car.direcao.girar_a_direita()
+    elif acao == ' ':
+        car.motor.acelerar()            
+    elif acao == '0':
+        car.motor.frear()
+
+
