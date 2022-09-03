c3f2360b1957a59c959a8dbed902fcd2935dddcf
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-09-03 17:57:49 -0300

    estudo sobre classes gettes e setter.

---
+++ Estudos programador lhama/alarme.py
@@ -0,0 +1,24 @@
+# Aula vídeo 04 - Orientação a Objetos em Python - Getters / Setters e Estados
+# conceito de estado.
+class Alarm:
+    def __init__(self, estado: bool) -> None:
+        self.__estado = estado
+    
+    def get_estado(self) -> bool:
+        return self.__estado
+
+    def set_estado(self, valor: bool) -> bool:
+        if isinstance(valor, bool):
+            self.__estado = valor
+
+clock = Alarm(True)
+# armado = clock.__estado Não funciona devido ao estado estar privado.
+armado = clock.get_estado()
+print(armado)
+clock.set_estado(False)
+armado = clock.get_estado()
+print(armado)
+
+clock.set_estado('não boleano')
+armado = clock.get_estado()
+print(armado)
