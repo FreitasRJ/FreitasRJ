15697633148fc3065338221c229f46bfc39a11f6
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-09-03 16:21:24 -0300

    estudo sobre metodo  privativo

---
+++ Estudos programador lhama/calculadora.py
@@ -0,0 +1,22 @@
+# aula video3 
+
+class Calculadora:
+
+    def calcular(self, op, num1, num2):
+        if op == "+":
+            return self.__adicionar(num1, num2)
+        if op == '-':
+            return self.__subtrair(num1, num2)
+
+        else: print ('Operação inválida')
+
+    def __adicionar(self, num1, num2): 
+        return num1 + num2
+
+    def __subtrair(self, num1, num2):
+        return num1 - num2
+
+
+calculadora = Calculadora() # sem metodo constrututor, basta estanciar.
+calculo = calculadora.calcular('*', 2, 5)
+print(calculo)
